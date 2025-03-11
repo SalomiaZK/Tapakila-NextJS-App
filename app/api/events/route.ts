17e9b7545fc91ab106d1b4a4cfde6541da453c96
RaJharit77 <hei.rajo.2@gmail.com>
2025-03-11 15:39:46 +0300

    chore: Update events and tickets api's

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,49 +1,50 @@
 
-import { prisma } from "@/lib/prisma"
+import { prisma } from "@/lib/prisma";
 
 
 export async function GET() {
     const events = await prisma.event.findMany(
     );
-    return new Response(JSON.stringify(events), {status:200, headers: {'Content-Type': 'application/json'}});
+    return new Response(JSON.stringify(events), { status: 200, headers: { 'Content-Type': 'application/json' } });
 }
 
-export async function POST(request : Request) {
-  try{
-    const body =await request.json()
-    const { event_name, event_place, event_category, event_date, event_description, event_organizer , event_id, event_image, admin_id} = body
-    if(!event_name|| !event_place || !event_category || !event_date){
-        return new Response(JSON.stringify({error: 'those field must be filled! '}))
-    }
-    else{
-        const newEv = await prisma.event.create({
-         data: {
-            event_name,
-            event_place,
-            event_category,
-            event_date,
-            event_description,
-            event_image,
-            event_organizer,
-            event_id,
-            admin_id// replace 'defaultAdmin' with the appropriate value
-         }
-            
-        })
+export async function POST(request: Request) {
+    try {
+        const body = await request.json()
+        const { event_name, event_place, event_category, event_date, event_description, event_organizer, event_id, event_image, admin_id } = body
+        if (!event_name || !event_place || !event_category || !event_date) {
+            return new Response(JSON.stringify({ error: 'those field must be filled! ' }))
+        }
+        else {
+            const newEv = await prisma.event.create({
+                data: {
+                    event_name,
+                    event_place,
+                    event_category,
+                    event_date,
+                    event_description,
+                    event_image,
+                    event_organizer,
+                    event_id,
+                    admin_id// replace 'defaultAdmin' with the appropriate value
+                }
 
-        return new Response(JSON.stringify(newEv) , {
-            status: 201
-        })
-    }}
-    catch(error){
-      console.error("Error while creating the event", error)
-      return new Response(JSON.stringify({ error : "Repository erro"}),
-      {status : 500} 
-    )
+            })
+
+            return new Response(JSON.stringify(newEv), {
+                status: 201
+            })
+        }
+    }
+    catch (error) {
+        console.error("Error while creating the event", error)
+        return new Response(JSON.stringify({ error: "Repository erro" }),
+            { status: 500 }
+        )
     }
 
-    finally{
-      await prisma.$disconnect()
+    finally {
+        await prisma.$disconnect()
     }
 }
 
--- app/api/tickets/[id]/route.ts
+++ app/api/tickets/[id]/route.ts
@@ -8,14 +8,14 @@ export async function GET(request: Request, { params }: { params: Promise<{ id: 
 
     const { id } = await params
     try {
-      const ticket = await prisma.ticket.findUnique({
-        where:{
-            ticket_id: id
-        },
-        include: {
-            user: true
-        }
-      }) 
+        const ticket = await prisma.ticket.findUnique({
+            where: {
+                ticket_id: id
+            },
+            include: {
+                user: true
+            }
+        })
 
         return new Response(JSON.stringify(ticket), { status: 200, headers: { 'Content-Type': 'application/json' } })
     }
