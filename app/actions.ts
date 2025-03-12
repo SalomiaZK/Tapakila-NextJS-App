5398d542119105069b6dba8576aa4a28124cc5a3
RaJharit77 <hei.rajo.2@gmail.com>
2025-03-12 21:06:23 +0300

    chore: Update auth.ts, prisma.ts and actions.ts files

--- app/actions.ts
+++ app/actions.ts
@@ -1,36 +1,76 @@
-"use server";
+"user server";
 
-import { neon } from "@neondatabase/serverless";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
+import { Type } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// acheter un ou beaucoup de ticket
+export async function BookATicket({ data }: { data: { userId: string, ticketNumber: number, requestType: "CANCEL" | "BOOK", ticketType: Type } }) {
+    try {
+        const { userId, ticketNumber, ticketType, requestType /**CANCEL | BOOK */ } = data
 
-export async function getEvents() {
-    const events = await prisma.event.findMany({
-        include: {
-            tickets: true,
-        },
-    });
-    return events;
-}
 
-export async function getEventById(eventId: string) {
-    const event = await prisma.event.findUnique({
-        where: {
-            event_id: eventId,
-        },
-        include: {
-            tickets: true,
-        },
-    });
-    return event;
-}
+        if (requestType == "BOOK") {
 
-export async function getData() {
-    if (!process.env.DATABASE_URL) {
-        throw new Error("DATABASE_URL is not defined");
+            const foundTickets = await prisma.ticket.findMany({
+                take: ticketNumber,
+                where: {
+                    ticket_status: "AVAILABLE",
+                    ticket_type: ticketType
+                }
+            })
+
+            if (foundTickets.length == 0) {
+                return new Response(JSON.stringify({ message: "no more ticket available for this type" }))
+            }
+
+            const book = await prisma.ticket.updateMany({
+                where: {
+                    ticket_id: {
+                        in: foundTickets.map(t => t.ticket_id)
+                    }
+                },
+                data: {
+                    ticket_status: 'SOLD',
+                    user_id: userId
+                }
+
+            })
+
+            return new Response(JSON.stringify(book), { status: 200 })
+
+        } else {
+            // si on veut annuler
+            const foundTickets = await prisma.ticket.findMany({
+                take: ticketNumber,
+                where: {
+                    user_id: userId,
+                    ticket_type: ticketType
+                }
+            })
+
+            if (foundTickets.length == 0) {
+                return new Response(JSON.stringify({ message: "you have booked no ticket of that type" }))
+            }
+            const cancel = await prisma.ticket.updateMany({
+                where: {
+                    ticket_id: {
+                        in: foundTickets.map(t => t.ticket_id)
+                    }
+                },
+                data: {
+                    ticket_status: "AVAILABLE",
+                    user_id: null
+                }
+            })
+
+            return new Response(JSON.stringify(cancel), { status: 200 })
+
+        }
+
+    } catch (e) {
+        console.error("Error while updating the ticket", e)
+    } finally {
+        await prisma.$disconnect()
     }
-    const sql = neon(process.env.DATABASE_URL);
-    const data = await sql`...`;
-    return data;
+
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -64,7 +64,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             return session
         },
 
-
     },
 
     pages: {
