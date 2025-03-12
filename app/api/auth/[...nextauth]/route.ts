9093c0c25e82a0a767dc22334b31caa69d4a7e58
salomia <salomia.razakarivony@gmail.com>
2025-03-12 20:05:30 +0300

    fixed: next-auth config with middleware

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,69 @@
-import { handlers } from "@/lib/auth";
-export const {GET, POST} = handlers
+import NextAuth from "next-auth";
+import authconfig from "@/lib/auth.config"; 
+import Credentials from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma"; 
+import { JWT } from "next-auth/jwt";
+
+export interface CustomUser {
+  id: string;
+  email: string;
+  name: string;
+  password: string;
+}
+
+const { handlers, signIn, signOut, auth } = NextAuth({
+  session: { strategy: "jwt" },
+  providers: [
+    ...authconfig.providers,
+    Credentials({
+      name: "Codev Provider",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: {
+            user_email: credentials?.email as string,
+          },
+        });
+
+        if (user && user.user_password === credentials.password) {
+          const customUser: CustomUser = {
+            id: user.user_id,
+            email: user.user_email,
+            name: user.user_name,
+            password: user.user_password,
+          };
+          return customUser;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }: { token: JWT; user?: any }) {
+      if (user) {
+        token.name = user.name;
+        token.email = user.email;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id as string;
+      session.user.email = token.email ?? "";
+      session.user.name = token.name;
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login", // Page de connexion personnalisée
+  },
+});
+
+// Exporte les handlers GET et POST
+export const { GET, POST } = handlers;
+
+// Exporte les fonctions pour les utiliser dans ton application
+export { signIn, signOut, auth };
--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,24 @@
-export { auth as middleware } from "@/lib/auth"
+import { auth } from "@/app/api/auth/[...nextauth]/route"; // Importe la fonction `auth` depuis ton fichier de configuration
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+    const {pathname} = new URL(req.url);
+
+    const publicRoutes = [
+        "/login",
+        "/signup",
+    ]
+
+    if(req.auth && publicRoutes.includes(pathname)){
+        return  NextResponse.redirect(new URL("/", req.url).toString(), 302);
+    }
+
+    if(!req.auth && !publicRoutes.includes(pathname)){
+        return  NextResponse.redirect(new URL("/login", req.url).toString(), 302);
+    }
+
+    return NextResponse.next()
+});
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login|signup|about).*)"]};
